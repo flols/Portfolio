68e86e9a044e237136f2314a69aeba2cb75cb615
flols <fflols22@gmail.com>
2024-07-22 16:56:33 +0200

    add title and button on home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,52 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
 
-export default function Home() {
+// components
+import Social from "@/components/Social";
+
+
+const Home = () => {
   return (
-    <main>homepage</main>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Developpeur Web</span>
+            <h1 className="h1 mb-6">
+              Bonjour, je suis <br />
+              <span className="text-accent">Florian Le Saint</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            {/* button and social */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social 
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex 
+                  justify-center items-center text-accent text-base hover:bg-accent
+                  hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          {/* photo */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default Home;
