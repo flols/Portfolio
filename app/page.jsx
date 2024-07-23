bf4de5ff896ad506a608684ba179de16467962fe
flols <fflols22@gmail.com>
2024-07-23 15:34:54 +0200

    Add Photo and Stats components on homepage

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,8 @@ import { FiDownload } from "react-icons/fi";
 
 // components
 import Social from "@/components/Social";
+import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 
 const Home = () => {
@@ -11,7 +13,7 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Developpeur Web</span>
             <h1 className="h1 mb-6">
               Bonjour, je suis <br />
@@ -42,9 +44,12 @@ const Home = () => {
             </div>
           </div>
           {/* photo */}
-          <div>photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 };
