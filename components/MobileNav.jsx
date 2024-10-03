e58e1e1a33805aaac9c8a47055c77fd296c7de1a
flols <fflols22@gmail.com>
2024-10-03 18:06:01 +0200

    update transition

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -7,70 +7,73 @@ import { CiMenuFries } from "react-icons/ci";
 import { useState, useEffect } from "react";
 
 const links = [
-    { 
-        name: "home", 
-        path: "/",
-    },
-    { 
-        name: "services", 
-        path: "/services",
-    },
-    { 
-        name: "resume", 
-        path: "/resume", 
-    },
-    { 
-        name: "work", 
-        path: "/work", 
-    },
-    { 
-        name: "contact", 
-        path: "/contact", 
-    },
-]
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
 
 const MobileNav = () => {
-    const pathname = usePathname();
-    const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
 
-    useEffect(() => {
-        setIsOpen(false);
-    }, [pathname]);
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
 
-    return (
-        <Sheet open={isOpen} onOpenChange={setIsOpen}>
-            <SheetTrigger className="flex justify-center items-center" onClick={() => setIsOpen(!isOpen)}>
-                <CiMenuFries className="text-[32px] text-accent" />
-            </SheetTrigger>
-            <SheetContent className="flex flex-col">
-                <div className="mt-32 mb-40 text-center text-2xl">
-                    <Link href="/">
-                        <h1 className="text-4xl font-semibold">
-                            Florian<span className="text-accent">.</span>
-                        </h1>
-                    </Link>
-                </div>
-                {/* nav*/}
-                <nav className="flex flex-col justify-center items-center gap-8">
-                    {links.map((link, index) => {
-                        return (
-                            <Link
-                                href={link.path}
-                                key={index}
-                                className={`${
-                                    link.path === pathname &&
-                                    "text-accent border-b-2 border-accent"
-                                } text-xl capitalize hover:text-accent transition-all`}
-                                onClick={() => setIsOpen(false)}
-                            >
-                                {link.name}
-                            </Link>
-                        );
-                    })}  
-                </nav>
-            </SheetContent>
-        </Sheet>
-    )
+  return (
+    <Sheet open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger
+        className="flex justify-center items-center"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              Florian<span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+        {/* nav*/}
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${
+                  link.path === pathname &&
+                  "text-accent border-b-2 border-accent"
+                } text-xl capitalize hover:text-accent transition-all`}
+                onClick={() => setIsOpen(false)}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
